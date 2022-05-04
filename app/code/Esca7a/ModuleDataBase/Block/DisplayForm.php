c89e9f1815fc41c876799e21e911fb393f0d1870
ESCA7A <pasha.esca1a@gmail.com>
2022-05-04 23:51:48 +0300

    check .question.txt for info

--- app/code/Esca7a/ModuleDataBase/Block/DisplayForm.php
+++ app/code/Esca7a/ModuleDataBase/Block/DisplayForm.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace Esca7a\ModuleDataBase\Block;
+
 use Esca7a\ModuleDataBase\Model\AllCategory;
 use Magento\Catalog\Model\ProductRepository;
+
 class DisplayForm extends \Magento\Framework\View\Element\Template
 {
     protected $productCollectionFactory;
-    protected $_categoryHelper;
+    protected $categoryHelper;
     public function __construct(
         \Magento\Catalog\Block\Product\Context $context,
         \Magento\Catalog\Helper\Category $categoryHelper,
@@ -22,20 +24,19 @@ class DisplayForm extends \Magento\Framework\View\Element\Template
      * Retrieve current store level 2 category
      *
      * @param bool|string $sorted (if true display collection sorted as name otherwise sorted as based on id asc)
-     * @param bool $asCollection (if true display all category otherwise display second level category menu visible category for current store)
+     * @param bool $asCollection (if true display all category otherwise display second level category menu visible
+     * category for current store)
      * @param bool $toLoad
      */
 
     public function getStoreCategories($sorted = false, $asCollection = false, $toLoad = true)
     {
-        return $this->_categoryHelper->getStoreCategories($sorted , $asCollection, $toLoad);
+        return $this->_categoryHelper->getStoreCategories($sorted, $asCollection, $toLoad);
     }
-        /*
-         * loop for Product category to template
-         */
+
     public function getCategoryList()
     {
-        return $categories = $this->getStoreCategories(false,false,true);
+        return $categories = $this->getStoreCategories(false, false, true);
     }
 
     public function getProductCollectionByCategories($ids)
--- app/code/Esca7a/ModuleDataBase/Controller/ProductRegistration/Index.php
+++ app/code/Esca7a/ModuleDataBase/Controller/ProductRegistration/Index.php
@@ -4,17 +4,17 @@ namespace Esca7a\ModuleDataBase\Controller\ProductRegistration;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
-	protected $_pageFactory;
-	public function __construct(
-		\Magento\Framework\App\Action\Context $context,
-		\Magento\Framework\View\Result\PageFactory $pageFactory)
-	{
-		$this->_pageFactory = $pageFactory;
-		return parent::__construct($context);
-	}
+    protected $_pageFactory;
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $pageFactory
+    ) {
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
 
-	public function execute()
-	{
-		return $this->_pageFactory->create();
-	}
+    public function execute()
+    {
+        return $this->_pageFactory->create();
+    }
 }
