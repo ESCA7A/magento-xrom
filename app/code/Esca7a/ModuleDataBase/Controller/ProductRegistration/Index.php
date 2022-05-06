94e9d913fdcd2536c56e95229cf365800870e718
ESCA7A <pasha.esca1a@gmail.com>
2022-05-06 16:22:24 +0300

    completed redirect for controller

--- app/code/Esca7a/ModuleDataBase/Controller/ProductRegistration/Index.php
+++ app/code/Esca7a/ModuleDataBase/Controller/ProductRegistration/Index.php
@@ -4,17 +4,25 @@ namespace Esca7a\ModuleDataBase\Controller\ProductRegistration;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
+    protected $session;
     protected $_pageFactory;
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $pageFactory
+        \Magento\Framework\View\Result\PageFactory $pageFactory,
+        \Magento\Customer\Model\Session $session
     ) {
         $this->_pageFactory = $pageFactory;
+        $this->session = $session;
         return parent::__construct($context);
     }
-
+    public function redirectGuest()
+    {
+        $resultRedirect = $this->resultRedirectFactory->create();
+        return $resultRedirect->setPath('home');
+    }
     public function execute()
     {
-        return $this->_pageFactory->create();
+
+        return $this->session->isLoggedIn() ? $this->_pageFactory->create() : $this->redirectGuest();
     }
 }
