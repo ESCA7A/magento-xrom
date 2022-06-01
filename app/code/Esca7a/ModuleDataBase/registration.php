eba634dfa1b3e0e879ca1c70e354313cd3acb2ed
Pavel Tankov <pasha.esca1a@gmail.com>
2022-06-01 19:02:26 +0300

    Vendor: Esca7a message: refactoring code with psr-12 standart

--- app/code/Esca7a/ModuleDataBase/registration.php
+++ app/code/Esca7a/ModuleDataBase/registration.php
@@ -1,7 +1,7 @@
 <?php
 
 \Magento\Framework\Component\ComponentRegistrar::register(
-	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	'Esca7a_ModuleDataBase',
-	__DIR__
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Esca7a_ModuleDataBase',
+    __DIR__
 );
